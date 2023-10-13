659dddffbad936c1c5fc540315f6555cb381a455
anderk222 <anderk222@gmail.com>
2023-10-12 20:57:10 -0500

    rutas principales

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'layout',
+    loadChildren: () => import('./layout/layout.module').then(file => file.LayoutModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(file => file.AuthModule)
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
