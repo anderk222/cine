30e19b088e6b60aa5b34b1bb9d168de6d12c63f7
anderk222 <anderk222@gmail.com>
2023-10-12 20:52:58 -0500

    Navbar de la aplicacion

---
+++ src/app/layout/navbar/navar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'writers-navar',
+  templateUrl: './navar.component.html',
+  styleUrls: ['./navar.component.css']
+})
+export class NavarComponent {
+
+}
