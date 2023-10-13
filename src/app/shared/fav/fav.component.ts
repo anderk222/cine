cface2a42e0b2664fe03258709dcc887523789d4
anderk222 <anderk222@gmail.com>
2023-10-12 20:49:11 -0500

    componente compartido de boton de favorito

---
+++ src/app/shared/fav/fav.component.ts
@@ -0,0 +1,24 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'writers-fav',
+  templateUrl: './fav.component.html',
+  styleUrls: ['./fav.component.css'],
+  standalone: true,
+  imports : [ NgIf ]
+})
+export class FavComponent {
+
+  @Input() active = false;
+  @Output() change = new EventEmitter<boolean>();
+
+  handlerChange(){
+
+    this.active = !this.active;
+
+    this.change.emit(this.active);
+
+  }
+
+}
